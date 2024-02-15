bc75b85b1fece4de974f1d2ae1af7c9639c629ea
Mario Pon <gxs.mpon@gmail.com>
2024-02-15 09:22:39 -0600

    feat: added logger

--- src/js/main/main.tsx
+++ src/js/main/main.tsx
@@ -1,10 +1,30 @@
+import React from 'react';
 import { Button } from '@adobe/react-spectrum';
+import CSInterface from '../lib/cep/csinterface';
+import { CSEvent } from '../lib/cep/csinterface';
+
 
 const Main = () => {
+  const [event, setEvent] = React.useState<string>('');
+
+  const csInterface = new CSInterface();
+
+  React.useEffect(() => {
+    const handleEvent = (event: CSEvent) => {
+      console.log(event);
+      setEvent(event.data);
+    };
+
+    csInterface.addEventListener('com.gxs.logger.log_json', handleEvent);
+
+    return () => csInterface.removeEventListener('com.gxs.logger.log_json', handleEvent, null);
+  }, []);
+
   return (
-    <div className="w-full h-full flex flex-col gap-2 justify-center items-center">
-      <h1 className='text-xl font-bold'>Welcome to Bolt CEP!</h1>
-      <Button variant='accent' onPress={() => alert('Hello World')} UNSAFE_className='font-sans font-normal pb-1'>Hello From React</Button>
+    <div className="w-full h-full flex flex-col gap-2 justify-start items-start p-2">
+      {
+        event && <h1 className='text-sm font-mono'>{event}</h1>
+      }
     </div>
   );
 };
