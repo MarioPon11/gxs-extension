7034f5011f64b7fc8406bfab332a77f775fb8ba4
Mario Pon <gxs.mpon@gmail.com>
2024-02-16 11:48:13 -0600

    fix: Added event noter

--- src/js/main/main.tsx
+++ src/js/main/main.tsx
@@ -8,7 +8,28 @@ const Main = () => {
 
   const csInterface = new CSInterface();
 
+  function setFlyoutMenu() {
+    const menuString = `<Menu>
+		<MenuItem Id="menuItemId1" Label="Settings" Enabled="true"/>
+		<MenuItem Id="menuItemId2" Label="Help" Enabled="true"/>
+	</Menu>`;
+    csInterface.setPanelFlyoutMenu(menuString);
+    csInterface.addEventListener(("com.gxs.logger.events.flyoutMenuClicked"), function (event: CSEvent) {
+
+      const data = JSON.parse(event.data);
+      console.log(data);
+      if (data.menuId === "menuItemId1") {
+        console.log("Settings clicked");
+      } else if (data.menuId === "menuItemId2") {
+        console.log("Help clicked");
+      }
+
+    });
+  }
+
   React.useEffect(() => {
+    setFlyoutMenu()
+
     const handleEvent = (event: CSEvent) => {
       console.log(event);
       setEvent(event.data);
@@ -18,6 +39,9 @@ const Main = () => {
 
     return () => csInterface.removeEventListener('com.gxs.logger.log_json', handleEvent, null);
   }, []);
+
+  // TODO: Flyout menu 
+  // TODO: Add a button to reload the extension in the flyout menu
 
   return (
     <div className="w-full h-full flex flex-col gap-2 justify-start items-start p-2">
